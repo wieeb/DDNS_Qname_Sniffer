f3c50b2bc5195326d5fb56387dddc1dbb2c9df21
Simon <77685734+wieeb@users.noreply.github.com>
2024-12-05 00:50:46 +0000

    Update dns.py

--- dns.py
+++ dns.py
@@ -3,17 +3,14 @@ from datetime import datetime
 from scapy.all import DNSQR, IP, UDP
 
 def sniffed_pkt(pkt, domains_to_listen, args):
-    if DNSQR in pkt:  # Asegurarse de que es una consulta DNS
+    if DNSQR in pkt:
         qname = pkt[DNSQR].qname.decode().strip()
         packet_time = datetime.fromtimestamp(pkt.time)
         timestamp_str = packet_time.strftime("%Y-%m-%d %H:%M:%S")
         qname_normalized = qname.lower().rstrip('.')
-
-        # Comprobar si el qname termina con alguno de los dominios de la lista
         if any(qname_normalized.endswith(domain.lower()) for domain in domains_to_listen):
             print(f"DNS Query Detected for Domain: {qname}")
             write_data(pkt, timestamp_str, args)
-
 def write_data(pkt, timestamp, args):
     src_ip = pkt[IP].src
     dst_ip = pkt[IP].dst
